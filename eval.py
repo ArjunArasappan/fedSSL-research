245c469d601c463dc5fd0c977c5de7b4b4a63e1e
root <arasarjun@gmail.com>
2024-08-01 17:24:52 +0000

    ssl train updates

--- eval.py
+++ eval.py
@@ -3,6 +3,7 @@ from model import SimCLRPredictor
 import glob
 import os
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 DEVICE = utils.DEVICE
 
@@ -10,7 +11,7 @@ class EvalMetric:
     def __init__(self, trainset, testset):
         self.reference = SimCLRPredictor(10, DEVICE, useResnet18=False).to(DEVICE)
         self.anchor_num = 100
-        
+        self.cross_entropy = nn.CrossEntropyLoss()
         self.trainset, self.testset = trainset, testset
         
         
@@ -20,7 +21,28 @@ class EvalMetric:
         self.reference.load_state_dict(state_dict, strict = True)
         
     def evaluateReference(self):
-        print(type(self.trainset))
+        train = DataLoader(self.trainset, batch_size = len(trainset))
+        test = DataLoader(self.testset, batch_size = len(trainset))
+        acc = []
+        
+        reference.eval()
+        with torch.no_grad():
+            for item in [train, test]:
+                x, labels = item[0]['img'], item[0]['label']
+                x, labels = x.to(DEVICE), labels.to(DEVICE)
+                
+                logits = self.reference(x)
+                values, predicted = torch.max(logits, 1)  
+                
+                total = labels.size(0)
+                loss = self.cross_entropy(logits, labels).item()
+                correct = (predicted == labels).sum().item()
+
+                acc.append((loss, correct / total))
+            
+        # print(f"Reference Train: loss - {acc[0][0]}")
+        print(f"Reference Train: accuracy - {acc[0][1]}")
+        print(f"Reference Test: accuracy - {acc[1][1]}")
 
         
         
