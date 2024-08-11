a57a6a1bccfafb48356d3a4b9fdc66153d966694
root <arasarjun@gmail.com>
2024-08-11 04:02:43 +0000

    push

--- transform.py
+++ transform.py
@@ -1,29 +1,42 @@
+import numpy as np
+import torch
+import torchvision
+from torch import nn
 from torchvision import transforms
+import PIL
+
+import random
+
+
 
 class SimCLRTransform:
     def __init__(self, size=32):
-        color_jitter = transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
+        s = 1
+        color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
         self.size = size
- 
-        self.base_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-            
+
+        self.augment_tranaform = [
             transforms.RandomResizedCrop(size=size),
             transforms.RandomHorizontalFlip(),
             transforms.RandomApply([color_jitter], p=0.8),
             transforms.RandomGrayscale(p=0.2),
-            ]
-        )
+        ]
         
-        self.test_transform = transforms.Compose([
-            transforms.ToTensor(),
+        if random.choice([True, False]):
+            self.augment_transform.append(torchvision.transforms.GaussianBlur(int(0.1 * size), sigma = random.uniform(0.1, 2)))
+
+                
+        self.augment_tranaform.append(transforms.ToTensor())
+
+    def __call__(self, x):
+
+        transform = transforms.Compose(self.augment_transform)
+        no_augment = self.test_transform()
+
+        return (no_augment(x), transform(x), transform(x))
+
+    def test_transform(self):
+        return transforms.Compose([
             transforms.Resize(self.size),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            transforms.ToTensor()
         ])
-    
-    def __call__(self, x, augmentData):
-        if not augmentData:
-            return self.test_transform(x)
-        
-        return (self.test_transform(x), self.base_transform(x), self.base_transform(x))
