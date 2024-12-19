5715f6fdf8d12e0a988a964d9d09adb5b54b6d59
Fiona Luo <fiona.luo@gmail.com>
2024-12-18 23:08:01 -0500

    push

--- log_files/visualizer.py
+++ log_files/visualizer.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 df = pd.read_csv('/home/harsh/arjun/fedSSL-research/log_files/visualization_data.csv')
 
+
+
+
 plt.figure(figsize=(10, 6))
-plt.plot(df['epoch'], df['accuracy'], label='accuracy column vs epoch', color='blue')
+plt.plot(df['epoch'],  df['accuracy'], label='accuracy column vs epoch', color='blue')
 plt.plot(df['epoch'], df['similarity'], label='similarity vs epoch', color='orange')
 plt.xlabel('epochs')
 plt.ylabel('values')
